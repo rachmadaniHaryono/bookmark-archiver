ae17a5fa7e2efad49ab0470a5f8ac01ddab9c74c
rachmadaniHaryono <foreturiga@gmail.com>
2018-04-26 09:43:56 +0800

    new: dev: server
    - only index for now
    - no archiving feature

---
+++ bookmark_archiver/server.py
@@ -0,0 +1,92 @@
+#!/usr/bin/env python3
+from datetime import datetime
+from logging.handlers import TimedRotatingFileHandler
+import logging
+import os
+import sys
+
+try:
+    from flask import Flask, send_from_directory
+    from flask.cli import FlaskGroup
+    import click
+except ImportError:
+    print(
+        '[X] Please install required package to use the server: '
+        'pip install bookmark-archiver[server]'
+    )
+    raise SystemExit(1)
+
+
+# permanent env config
+os.environ['BOOKMARK_ARCHIVER_USE_DEFAULT_CONFIG'] = 'True'
+os.environ['BOOKMARK_ARCHIVER_CHG_ROOT_FOLDER'] = 'False'
+
+
+def create_app(script_info=None):
+    """create app."""
+    app = Flask(__name__)
+    # logging
+    log_file = 'bookmark_archiver.log'
+    file_handler = TimedRotatingFileHandler(log_file, 'midnight')
+    file_handler.setLevel(logging.WARNING)
+    file_handler.setFormatter(logging.Formatter('<%(asctime)s> <%(levelname)s> %(message)s'))
+    app.logger.addHandler(file_handler)
+    # reloader
+    reloader = app.config['TEMPLATES_AUTO_RELOAD'] = \
+        bool(os.getenv('BOOKMARK_ARCHIVER_RELOADER')) or app.config['TEMPLATES_AUTO_RELOAD']
+    if reloader:
+        app.jinja_env.auto_reload = True
+    # app config
+    app.config['SECRET_KEY'] = os.getenv('BOOKMARK_ARCHIVER_SECRET_KEY') or os.urandom(24)
+    app.config['WTF_CSRF_ENABLED'] = False
+    # debug
+    debug = app.config['DEBUG'] = bool(os.getenv('BOOKMARK_ARCHIVER_DEBUG')) or app.config['DEBUG']
+    if debug:
+        app.config['DEBUG'] = True
+        app.config['LOGGER_HANDLER_POLICY'] = 'debug'
+        logging.basicConfig(level=logging.DEBUG)
+        # pprint.pprint(app.config)
+        print('Log file: {}'.format(log_file))
+
+
+    @app.shell_context_processor
+    def shell_context():
+        return {'app': app}
+
+    @app.template_filter('from_timestamp')
+    def datetime_from_timestamp(s):
+        return datetime.fromtimestamp(float(s))
+
+    from . import views
+    json_file = views.get_json_file()
+    archive_folder = views.get_archive_folder()
+    # import is here to avoid program asking to create default config when no config exist
+    # routing
+    app.add_url_rule('/', 'index', views.index)  # NOQA
+    app.add_url_rule(
+        '/index.json', 'index_json',
+        lambda : send_from_directory(
+            os.path.dirname(os.path.abspath(json_file)),
+            os.path.basename(json_file)
+        )
+    )
+    app.add_url_rule(
+        '/archive/<timestamp>/favicon.ico', 'favicon',
+        lambda timestamp: send_from_directory(
+            os.path.join(os.path.abspath(archive_folder), timestamp),
+            'favicon.ico'
+        )
+    )
+    # app.add_url_rule('/archive/<int:timestamp>/index.html', 'link_index', views.link_index)  # NOQA
+    # app.add_url_rule('/archive/<int:timestamp>/index.json', 'link_index_json', views.link_index_json)  # NOQA
+    return app
+
+
+@click.group(cls=FlaskGroup, create_app=create_app)
+def cli():
+    """This is a management script for application."""
+    pass
+
+
+if __name__ == '__main__':
+    cli()
---
+++ bookmark_archiver/views.py
@@ -0,0 +1,40 @@
+import os
+import json
+from datetime import datetime
+
+from flask import render_template, send_from_directory
+
+from . import config
+
+def get_json_file():
+    if config.config._sections['archive']['html folder'] == '%(dir)s/html' \
+            and config.ARCHIVE_DIR == '':
+        json_file = os.path.join('html', 'index.json')
+    else:
+        json_file = os.path.join(config.HTML_FOLDER, 'index.json')
+    return json_file
+
+
+def get_archive_folder():
+    if config.config._sections['archive']['html folder'] == '%(dir)s/html' \
+            and config.config._sections['archive']['archive folder'] == '%(html folder)s/archive' \
+            and config.ARCHIVE_DIR == '':
+        archive_folder = os.path.join('html', 'archive')
+    else:
+        archive_folder = os.path.join(config.ARCHIVE_FOLDER, 'archive')
+    return archive_folder
+
+
+def index():
+    json_file = get_json_file()
+    with open(json_file) as f:
+        json_data = json.load(f)
+    return render_template(
+        'bookmark_archiver/index.html',
+        time_updated=datetime.fromtimestamp(float(json_data['updated'])),
+        num_links=json_data['num_links'],
+        entries=json_data['links'],
+        git_sha=config.GIT_SHA,
+        short_git_sha=config.GIT_SHA[:7] if config.GIT_SHA else '',
+        footer_info=config.FOOTER_INFO,
+    )
